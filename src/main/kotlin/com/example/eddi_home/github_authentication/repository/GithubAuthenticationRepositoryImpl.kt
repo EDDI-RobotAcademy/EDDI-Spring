003e37a7fac9dc2929b97be10fabce9620710431
Sanghoon Lee <gcccompil3r@gmail.com>
2024-09-10 16:04:17 +0900

    feat: Access Token으로 User Info 요청 [EHBS-3]

--- src/main/kotlin/com/example/eddi_home/github_authentication/repository/GithubAuthenticationRepositoryImpl.kt
+++ src/main/kotlin/com/example/eddi_home/github_authentication/repository/GithubAuthenticationRepositoryImpl.kt
@@ -1,11 +1,21 @@
 package com.example.eddi_home.github_authentication.repository
 
+import com.example.eddi_home.github_authentication.service.response.GithubUserInfoResponse
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Repository
 import org.springframework.web.client.RestTemplate
 
 @Repository
-class GithubAuthenticationRepositoryImpl : GithubAuthenticationRepository {
+class GithubAuthenticationRepositoryImpl(
+    private val restTemplate: RestTemplate
+) : GithubAuthenticationRepository {
 
     @Value("\${github.client.id}")
     private lateinit var clientId: String
@@ -25,10 +35,32 @@ class GithubAuthenticationRepositoryImpl : GithubAuthenticationRepository {
             "redirect_uri" to redirectUri
         )
 
-        // RestTemplate을 사용하여 액세스 토큰 요청
-        val restTemplate = RestTemplate()
         val response = restTemplate.postForEntity(url, params, String::class.java)
 
-        return response.body ?: throw RuntimeException("Failed to retrieve access token")
+        return response.body ?: throw RuntimeException("access token 획득 실패")
+    }
+
+    override fun requestUserInfo(accessToken: String): GithubUserInfoResponse {
+
+        val cleanedToken = accessToken.split("&")[0].split("=")[1]
+        println("cleanedToken: $cleanedToken")
+
+        val headers = HttpHeaders()
+        headers.set("Authorization", "Bearer $cleanedToken")
+
+        val entity = HttpEntity<String>("", headers)
+        val response: ResponseEntity<String> = restTemplate.exchange(
+            "https://api.github.com/user",
+            HttpMethod.GET,
+            entity,
+            String::class.java
+        )
+
+        val responseBody = response.body ?: throw RuntimeException("user info 획득 실패")
+
+        val objectMapper = jacksonObjectMapper()
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return objectMapper.readValue(responseBody)
     }
 }
